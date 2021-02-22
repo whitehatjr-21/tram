ebe484f97cffb986709949023aed3e08390218de
whitehatjr-21 <72784239+whitehatjr-21@users.noreply.github.com>
2021-02-22 16:22:24 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,8 +6,7 @@ const Constraint = Matter.Constraint;
 var myEngine, myWorld;
 var bg;
 var ground;
-var boggie1,boggie2,boggie3;
-var chain1;
+
 var trainSound 
 var crashSound
 var flag = 0;
@@ -23,21 +22,9 @@ function setup() {
   myWorld= myEngine.world;
 
   ground = new Ground(600,380,1200,20);
-boggie1=new Boggie(50,170,50,50);
-boggie2=new Boggie(150,170,50,50);
-boggie3=new Boggie(250,170,50,50);
-boggie4=new Boggie(350,170,50,50);
-boggie5=new Boggie(450,170,50,50);
-boggie6=new Boggie(550,170,50,50);
 
 
 
-rock1= new Rock(1100,200,100,100);
-chain1 = new Chain(boggie1.body,boggie2.body);
-chain2 = new Chain(boggie2.body,boggie3.body);
-chain3 = new Chain(boggie3.body,boggie4.body);
-chain4 = new Chain(boggie4.body,boggie5.body);
-chain5 = new Chain(boggie5.body,boggie6.body);
 
 
 }
@@ -46,20 +33,7 @@ function draw() {
   background(bg);  
   Engine.update(myEngine);
 
-  boggie1.show();
-  boggie2.show();
-  boggie3.show();
-  boggie4.show();
-  boggie5.show();
-  boggie6.show();
-
-  rock1.show();
   
-  chain1.show();
-  chain2.show();
-  chain3.show();
-  chain4.show();
-  chain5.show();
   var collision = Matter.SAT.collides(boggie6.body,rock1.body);
   if(collision.collided)
   {
